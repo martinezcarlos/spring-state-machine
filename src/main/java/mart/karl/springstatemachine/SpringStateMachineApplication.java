9336f2b6021eec6b3eab37bb9fe31636c033d473
martinezcarlos <ingeniero.carlos.martinez@gmail.com>
2020-01-16 22:35:06 +0100

    Initial commit

---
+++ src/main/java/mart/karl/springstatemachine/SpringStateMachineApplication.java
@@ -0,0 +1,12 @@
+package mart.karl.springstatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStateMachineApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(SpringStateMachineApplication.class, args);
+  }
+}
---
+++ src/test/java/mart/karl/springstatemachine/SpringStateMachineApplicationTests.java
@@ -0,0 +1,12 @@
+package mart.karl.springstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
