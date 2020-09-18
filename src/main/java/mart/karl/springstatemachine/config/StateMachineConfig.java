c9aeb87509ae7fb8f2e989dd9a09b3ad4c6d1a3b
Carlos Martínez <carlosmm@ext.inditex.com>
2020-09-18 19:46:20 +0200

    Example of a Guard.

--- src/main/java/mart/karl/springstatemachine/config/StateMachineConfig.java
+++ src/main/java/mart/karl/springstatemachine/config/StateMachineConfig.java
@@ -12,6 +12,7 @@ import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.guard.Guard;
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
@@ -64,6 +65,7 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
         .target(PaymentState.NEW)
         .event(PaymentEvent.PRE_AUTHORIZE)
         .action(preAuthAction())
+        .guard(paymentIdGuard())
         .and()
         .withExternal()
         .source(PaymentState.NEW)
@@ -91,6 +93,11 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
         .source(PaymentState.PRE_AUTH)
         .target(PaymentState.AUTH_ERROR)
         .event(PaymentEvent.AUTH_DECLINED);
+  }
+
+  private Guard<PaymentState, PaymentEvent> paymentIdGuard() {
+    return stateContext ->
+        stateContext.getMessageHeader(PaymentServiceImpl.PAYMENT_ID_HEADER) != null;
   }
 
   private Action<PaymentState, PaymentEvent> preAuthAction() {
